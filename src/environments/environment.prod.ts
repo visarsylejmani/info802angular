e62c54f69f5648c84e5e8bce70ee45e0594c4615
Visar <Visar@DESKTOP-8A09G93>
2021-04-01 15:06:41 +0200

    v1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDO3GRg3k_R4Xlge5HtB5MthKxJMhSzpQs",
+    authDomain: "info802-d8e44.firebaseapp.com",
+    databaseURL: "https://info802-d8e44-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "info802-d8e44",
+    storageBucket: "info802-d8e44.appspot.com",
+    messagingSenderId: "167671150783",
+    appId: "1:167671150783:web:16089bbe457792c2bd6e0a",
+    measurementId: "G-NXCBD8YYB5"
+  }
 };
