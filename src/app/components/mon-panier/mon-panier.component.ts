a5c47dafd0ffc3d8bc1107a49e348b31d0483c24
Visar <Visar@DESKTOP-8A09G93>
2021-04-18 07:04:18 +0200

    fix:quand achat on vide le panier

--- src/app/components/mon-panier/mon-panier.component.ts
+++ src/app/components/mon-panier/mon-panier.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { FormBuilder } from '@angular/forms';
 import { AuthService } from 'src/app/shared/services/auth.service';
-import { ClientAPI,Commande,CompteEnBanque} from 'src/app/shared/services/clientapi.service';
+import { ClientAPI, Commande, CompteEnBanque } from 'src/app/shared/services/clientapi.service';
 import { PanierService } from 'src/app/shared/services/panier.service';
 import { Product } from 'src/app/shared/services/product';
 
-declare function soapRequest(km,poids):any;
+declare function soapRequest(km, poids): any;
 
 @Component({
   selector: 'app-mon-panier',
@@ -19,64 +19,68 @@ export class MonPanierComponent implements OnInit {
     numero: '',
   });
 
-  maCarte:CompteEnBanque;
+  maCarte: CompteEnBanque;
 
   clientApi = new ClientAPI();
   constructor(
     public afs: AngularFirestore,
     public authService: AuthService,
     public panier: PanierService,
-    public formBuilder:FormBuilder,
-    ) { }
+    public formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
   }
 
-  async verifierCarteBanque(){
+  async verifierCarteBanque() {
     console.log(this.CarteBanqueForm.value["numero"]);
     this.maCarte = await this.clientApi.query(this.CarteBanqueForm.value["numero"])
-    if(this.maCarte!=null){
+    if (this.maCarte != null) {
       console.log(this.maCarte)
       const div = document.getElementById("ResponseFromServer");
-      div.className ="alert alert-success";
-      div.innerHTML='Carte Valide'
-      document.getElementById("buttonAcheter").className="visible btn btn-success"
-    }else{
+      div.className = "alert alert-success";
+      div.innerHTML = 'Carte Valide'
+      document.getElementById("buttonAcheter").className = "visible btn btn-success"
+    } else {
       const div = document.getElementById("ResponseFromServer");
-      div.className ="alert alert-danger";
-      div.innerHTML='Carte Invalide'
-      document.getElementById("buttonAcheter").className="invisible btn btn-success"
+      div.className = "alert alert-danger";
+      div.innerHTML = 'Carte Invalide'
+      document.getElementById("buttonAcheter").className = "invisible btn btn-success"
 
     };
   }
-  
-  prixDeLivraison(){
-    soapRequest(Math.floor(Math.random() * (100 - 20 + 1) + 0),Math.floor(Math.random() * (50 - 20 + 1) + 0));
+
+  prixDeLivraison() {
+    soapRequest(Math.floor(Math.random() * (100 - 20 + 1) + 0), Math.floor(Math.random() * (50 - 20 + 1) + 0));
   }
 
-  acheter(){
+  acheter() {
     const commande = new Commande;
 
     const r = document.getElementById("ResponseFromSoap").innerHTML;
     const divRes = document.getElementById("ResponseAchat");
+    const divAlert = document.getElementById("AlertForAll");
+    
     commande.init({
-      iDacheteur:this.maCarte.id,
-      iDvendeur:this.panier.mesProduits[0].vendeur,
-      montant:(this.panier.total+parseFloat(document.getElementById("ResponseFromSoap").innerHTML))
-     });
-     console.log(commande)
-     this.clientApi.commande(commande).then(()=>{
-      divRes.className ="alert alert-success";
+      iDacheteur: this.maCarte.id,
+      iDvendeur: this.panier.mesProduits[0].vendeur,
+      montant: (this.panier.total + parseFloat(document.getElementById("ResponseFromSoap").innerHTML))
+    });
+    console.log(commande)
+    this.clientApi.commande(commande).then(() => {
+      divRes.className = "alert alert-success";
       divRes.innerHTML = "Payment Réussi"
+      divAlert.className = "alert alert-success";
+      divAlert.innerHTML = "Payment Réussi";
       this.panier.mesProduits = new Array<Product>();
-     }).catch((err)=>{
-      divRes.className ="alert alert-danger";
+    }).catch((err) => {
+      divRes.className = "alert alert-danger";
       divRes.innerHTML = "Payment Echoué"
-     });
+    });
   }
-  vide(){
-    return this.panier.mesProduits.length==0;
+  vide() {
+    return this.panier.mesProduits.length == 0;
   }
-  
+
 
 }
